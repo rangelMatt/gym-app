c4ca3a10408a79823570be4b6dedacb8c0bb84c4
Matt Rangel <mattrangel@gmail.com>
2022-09-02 16:04:19 -0700

    passing props from footer and header to app

--- src/App.js
+++ src/App.js
@@ -3,14 +3,17 @@ import './App.css';
 import Header from './components/header';
 import Footer from './components/footer';
 
+function createAlert() {
+  alert('Hello. You clicked me');
+}
 
 function App() {
   return (
     <div className="App">
-      <Header info="This is MY message"
-      myNumber="6"/>
+      <Header info="This is MY message"/>
       <p>main content</p>
-      <Footer trademark="page by Matt"/>
+      <Footer trademark="page by Matt"
+      myAlert={createAlert}/>
     </div>
   );
 }
--- src/components/footer.js
+++ src/components/footer.js
@@ -1,8 +1,22 @@
 import React, { Component } from 'react';
 
 class Footer extends Component {
+
+  changed() {
+    console.log('Footer changed')
+  }
+
   render() {
-    return <h2>{this.props.trademark}</h2>
+    return (
+      <div>
+        <h2 onClick={this.props.myAlert}>
+          {this.props.trademark}
+        </h2>
+        <input onChange={this.changed}type="text" />
+      </div>
+    )
+
+
   }
 }
 
