bf378aae9c6bdebdf2671bf02ce136ace06cd66f
Matt Rangel <mattrangel@gmail.com>
2022-09-07 12:43:59 -0700

    added animal map and key

--- src/components/footer.js
+++ src/components/footer.js
@@ -20,23 +20,18 @@ class Footer extends Component {
   }
 
   render() {
+
+    const animals = ['cat', 'dog', 'horse'];
+
     return (
       <div>
-
-
-        {this.state.isLogin ? (
-          <React.Fragment>
-            <h2 onClick={this.props.myAlert}>
-              {this.props.trademark}
-            </h2>
-            <input value={this.state.name}
-              onChange={this.changed} type="text" />
-          </React.Fragment>
-        ) : (<React.Fragment>
-          <h2>You can't see this content</h2>
-          <h2>Please login to see content</h2>
-        </React.Fragment>
-        )}
+        {animals.map(animal => {
+          return (
+            <div key={animal}>
+              <h1 >{animal}</h1>
+            </div>
+          );
+        })}
       </div>
     )
 
